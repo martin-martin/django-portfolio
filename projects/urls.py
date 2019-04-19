192f7ec077c61b5ad4ab2740aec574247ab1b7d7
martin-martin <breuss.martin@gmail.com>
2019-04-19 13:55:12 +0800

    restructure URLs

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 
+app_name = 'projects'
+
 urlpatterns = [
     path('', views.index, name='projects'),
     path('<int:pk>', views.detail, name='project_detail'),
--- projects/views.py
+++ projects/views.py
@@ -6,6 +6,7 @@ def index(request):
     projects = Project.objects.all()
     return render(request, 'projects/index.html', {'projects': projects})
 
+
 def detail(request, pk):
     project = Project.objects.get(pk=pk)
     return render(request, 'projects/detail.html', {'project': project})
